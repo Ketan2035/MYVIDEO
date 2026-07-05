2b5ef8ba4c0918338805df64019ed03d8e2b5f9a
ketan2035 <ketankumar147856@gmail.com>
2026-07-05 09:13:06 +0530

    auth and web socket done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,33 +1,40 @@
 import express from "express";
-
-import mongoose from "mongoose";
-
-import { connectToSoket } from "./controllers/socketManager.js";
-
 import { createServer } from "node:http";
-import { Server } from "socket.io";
+import { connectToSocket } from "./controllers/socketManager.js";
+import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/users.routes.js";
+
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
-const io = connectToSoket(server);
 
-app.set("port", process.env.PORT || 8000);
+connectToSocket(server);
+
 app.use(cors());
-app.use(express.json({limit:"40kb"}));
-app.use(express.urlencoded({limit:"40kb" ,extended:"true"}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ extended: true, limit: "40kb" }));
 
-app.get("/", (req, res) => {
-  res.send("hello ");
-});
+app.use("/api/v1/users", userRoutes);
+
+const PORT = process.env.PORT || 8000;
 
 const start = async () => {
-  const connectionDb = await mongoose.connect(
-    "mongodb+srv://ketanrs59_db_user:NQ2ZC6AZdcLVTvwq@cluster0.ncc2qkl.mongodb.net/"
-  );
-  console.log(`mongoose connected to db Host: ${connectionDb.connection.host}`);
-  server.listen(app.get("port"), () => {
-    console.log("listening on port 8000");
-  });
+    try {
+        const connectionDb = await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log(`MongoDB Connected: ${connectionDb.connection.host}`);
+
+        server.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+
+    } catch (err) {
+        console.error("MongoDB Connection Error:", err.message);
+        process.exit(1);
+    }
 };
+
 start();
